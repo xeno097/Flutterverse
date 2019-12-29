05726db5493d973c6b01af8c1b0c0f9cba0d4c1a
xeno097 <xeno097.cp@gmail.com>
2019-12-29 19:46:02 -0400

    started working on search page

--- xeno097 workspace/instagram_clone/lib/pages/search_page/search_page.dart
+++ xeno097 workspace/instagram_clone/lib/pages/search_page/search_page.dart
@@ -4,8 +4,17 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextField(
+          decoration: InputDecoration(
+            icon: Icon(Icons.search),
+            hintText: "Search",
+          ),
+        ),
+      ),
       body: Center(
-        child: Text('search'),
+        child: TextField(),
       ),
     );
   }
